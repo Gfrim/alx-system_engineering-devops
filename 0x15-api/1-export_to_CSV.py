b0b3cef2ae7552389b76af12545185cf558b3e38
Gfrim <84197818+Gfrim@users.noreply.github.com>
2023-02-08 18:18:12 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-'''Export to CSV'''
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
-if __name__ == '__main__':
-    param = sys.argv[1]
-    user_response = requests.get(f'https://jsonplaceholder.
-        typicode.com/users/{param}').json()
-    todo_response = requests.get(f'https://jsonplaceholder.
-        typicode.com/todos').json()
-    username = response['username']
 
-    with open('{}.csv', 'w') as filename:
-        for task in todo_response:
-            filename.write('"{}","{}","{}","{}"\n'
-                        .format(param, username, task.get('completed'),
-                                task.get('title')))
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
