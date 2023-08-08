7b7ce193669cff52a5d142ab1ddc8358e9e2e781
Gfrim <gfrim77@gmail.com>
2023-08-08 15:33:51 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
 from requests import get
 
 
 def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
+
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
@@ -12,13 +21,13 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
-    all_data = response.json()
+    results = response.json()
 
     try:
-        raw1 = all_data.get('data').get('children')
+        my_data = results.get('data').get('children')
 
-        for i in raw1:
+        for i in my_data:
             print(i.get('data').get('title'))
 
-    except:
+    except Exception:
         print("None")
